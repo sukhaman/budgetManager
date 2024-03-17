06297ddf9506009c72554643de3f806538cc5981
Sukhaman Singh <sukhamandev@gmail.com>
2024-03-17 13:32:17 -0400

    basic UI for login added with tests

--- BudgetManager/BudgetManager/Login/Views/LoginVC.swift
+++ BudgetManager/BudgetManager/Login/Views/LoginVC.swift
@@ -6,13 +6,104 @@ import UIKit
 
 class LoginVC: UIViewController {
     
+    let loginView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.textColor = .white
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+        ]
+        let attributedPlaceholder = NSAttributedString(string: "Email", attributes: attributes)
+
+        // Assign the attributed placeholder text to the text field
+        textField.attributedPlaceholder = attributedPlaceholder
+        textField.backgroundColor = UIColor(resource: .textFieldBG)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.isSecureTextEntry = true
+        textField.borderStyle = .roundedRect
+        textField.textColor = .white
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+        ]
+        let attributedPlaceholder = NSAttributedString(string: "Password", attributes: attributes)
+
+        // Assign the attributed placeholder text to the text field
+        textField.attributedPlaceholder = attributedPlaceholder
+        textField.backgroundColor = UIColor(resource: .textFieldBG)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let forgotPasswordButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Forgot Password?", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let signInButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign in", for: .normal)
+        button.backgroundColor = UIColor(resource: .secondaryGreen)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 22.5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureViewController()
-    }
-    
-    private func configureViewController() {
-        view.backgroundColor = UIColor(resource: .primaryGreen)
+        view.backgroundColor  = UIColor(resource: .primaryGreen)
+        view.addSubview(loginView)
+        loginView.addSubview(emailTextField)
+        loginView.addSubview(passwordTextField)
+        loginView.addSubview(forgotPasswordButton)
+        loginView.addSubview(signInButton)
+        let screenWidth = view.frame.width
         
+        
+        NSLayoutConstraint.activate([
+            // Login View
+            loginView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.widthAnchor.constraint(equalToConstant: UITraitCollection.current.horizontalSizeClass == .compact ? (screenWidth * 0.9) : 440),
+            loginView.heightAnchor.constraint(equalToConstant: UITraitCollection.current.horizontalSizeClass == .compact ? (screenWidth * 0.9) : 440),
+            
+            // Email Text Field
+            emailTextField.topAnchor.constraint(equalTo: loginView.topAnchor, constant: 50),
+            emailTextField.leadingAnchor.constraint(equalTo: loginView.leadingAnchor, constant: 20),
+            emailTextField.trailingAnchor.constraint(equalTo: loginView.trailingAnchor, constant: -20),
+            emailTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            // Password Text Field
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordTextField.leadingAnchor.constraint(equalTo: loginView.leadingAnchor, constant: 20),
+            passwordTextField.trailingAnchor.constraint(equalTo: loginView.trailingAnchor, constant: -20),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            // Forgot Password Button
+            forgotPasswordButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor),
+            forgotPasswordButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor, constant: -20),
+            forgotPasswordButton.widthAnchor.constraint(equalToConstant: 150),
+            forgotPasswordButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            // Sign In Button
+            signInButton.topAnchor.constraint(equalTo: forgotPasswordButton.bottomAnchor, constant: 20),
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor, constant: 20),
+            signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor, constant: -20),
+            signInButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
