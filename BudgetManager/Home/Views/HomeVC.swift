107c51f21dd0ff0835a84dc3204f0ec4cc71422a
Sukhaman Singh <sukhamandev@gmail.com>
2024-03-22 22:25:10 -0400

    Investment view added

--- BudgetManager/Home/Views/HomeVC.swift
+++ BudgetManager/Home/Views/HomeVC.swift
@@ -18,6 +18,17 @@ class HomeVC: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    
+    let creditView: CreditView = {
+        let view = CreditView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    let investmentView: InvestmentView = {
+        let view = InvestmentView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = BudgetManagerColors.primaryGreen.color
@@ -33,6 +44,8 @@ class HomeVC: UIViewController {
     private func configureViewController() {
        self.view.addSubview(headerView)
         view.addSubview(cashView)
+        view.addSubview(creditView)
+        view.addSubview(investmentView)
         
         NSLayoutConstraint.activate([
         // Header view constraints
@@ -41,10 +54,20 @@ class HomeVC: UIViewController {
             headerView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
             headerView.heightAnchor.constraint(equalToConstant: 80),
             
-            cashView.topAnchor.constraint(equalTo: self.headerView.bottomAnchor),
+            cashView.topAnchor.constraint(equalTo: self.headerView.bottomAnchor, constant: 20),
             cashView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor,constant: 20),
             cashView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor,constant: -20),
             cashView.heightAnchor.constraint(equalToConstant: 50),
+            
+            creditView.topAnchor.constraint(equalTo: self.cashView.bottomAnchor, constant: 20),
+            creditView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor,constant: 20),
+            creditView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor,constant: -20),
+            creditView.heightAnchor.constraint(equalToConstant: 50),
+            
+            investmentView.topAnchor.constraint(equalTo: self.creditView.bottomAnchor, constant: 20),
+            investmentView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor,constant: 20),
+            investmentView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor,constant: -20),
+            investmentView.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
 }
