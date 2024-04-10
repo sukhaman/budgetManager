236d76486c00fbdcbfd57ecf0795309d95f69306
Sukhaman Singh <sukhamandev@gmail.com>
2024-04-09 21:47:50 -0400

    AddTransactionVC added

---
+++ BudgetManager/Transaction/VIews/AddTransactionVC.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Sukhaman Singh on 4/9/24.
+//
+
+import UIKit
+
+class AddTransactionVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = BudgetManagerColors.primaryGreen.color
+    }
+}
